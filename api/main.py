0210bdc6ac25b4d49e6d2c29e32af5d714eb4a07
sampathub <sampathub89@gmail.com>
2026-03-30 16:26:59 +0530

    Update main.py

--- api/main.py
+++ api/main.py
@@ -1,11 +1,11 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import swisseph as swe
+from datetime import datetime, timedelta
 
 app = Flask(__name__)
 CORS(app)
 
-# නැකත් 27 ක ලැයිස්තුව
 NAKSHATRAS = [
     "Ashwini", "Bharani", "Krithika", "Rohini", "Mrigashira", "Arudra", 
     "Punarvasu", "Pushya", "Ashlesha", "Magha", "Purva Phalguni", "Uttara Phalguni", 
@@ -25,26 +25,32 @@ def calculate():
         lat = float(request.args.get('lat'))
         lon = float(request.args.get('lon'))
 
-        # UTC Offset (GMT +5.5)
-        utc_hour = h + (mi / 60.0) - 5.5
-        jd_ut = swe.julday(y, m, d, utc_hour)
+        # --- නිවැරදි UTC ගණනය කිරීම ---
+        # ලංකාවේ වේලාවෙන් පැය 5 මිනිත්තු 30 ක් අඩු කිරීම
+        local_dt = datetime(y, m, d, h, mi)
+        utc_dt = local_dt - timedelta(hours=5, minutes=30)
+        
+        # UTC දිනය සහ වේලාව ලබා ගැනීම
+        jd_ut = swe.julday(utc_dt.year, utc_dt.month, utc_dt.day, 
+                           utc_dt.hour + utc_dt.minute/60.0 + utc_dt.second/3600.0)
         
         swe.set_sid_mode(swe.SIDM_LAHIRI)
         
-        # 1. ලග්නය ගණනය කිරීම (Ascendant)
+        # 1. ලග්නය ගණනය කිරීම
+        # 64 = FLG_SIDEREAL
         res, ascmc = swe.houses_ex(jd_ut, lat, lon, b'O', 64)
         zodiac_signs = ["Mesha", "Vrushaba", "Mithuna", "Kataka", "Sinha", "Kanya", 
                         "Thula", "Vrushchika", "Dhanu", "Makara", "Kumbha", "Meena"]
+        
+        # ධනු රාශිය (Dhanu) අංශක 240 සිට 270 දක්වා වේ.
         lagnaya = zodiac_signs[int(ascmc[0] / 30)]
 
-        # 2. නැකත ගණනය කිරීම (Moon's position)
-        # 1 යනු චන්ද්‍රයා (Moon) සඳහා වන අංකයයි
+        # 2. නැකත ගණනය කිරීම
         moon_pos, retire = swe.calc_ut(jd_ut, 1, 64)
-        moon_degree = moon_pos[0] # චන්ද්‍රයා සිටින අංශකය
+        moon_degree = moon_pos[0]
         
-        # නැකතක් අංශක 13.3333 කින් සමන්විත වේ (360 / 27)
         nakshatra_index = int(moon_degree / (360 / 27))
-        nakshata = NAKSHATRAS[nakshatra_index]
+        nakshata = NAKSHATRAS[nakshatra_index % 27]
         
         return jsonify({
             "lagnaya": lagnaya,
